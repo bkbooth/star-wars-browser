02458f13084fd4c3c2afe71de95dbde6c25bd281
Ben Booth <bkbooth@gmail.com>
2018-06-07 14:22:15 +1000

    Move people vuex module logic to a module factory
    Create vuex modules for each Star Wars API endpoint

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import createLogger from 'vuex/dist/logger'
-import people from './modules/people'
+import createModule from './create-module'
 
 const DEBUG = process.env.NODE_ENV !== 'production'
 
@@ -9,7 +9,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    people,
+    people: createModule('people'),
+    planets: createModule('planets'),
+    films: createModule('films'),
+    species: createModule('species'),
+    vehicles: createModule('vehicles'),
+    starships: createModule('starships'),
   },
   strict: DEBUG,
   plugins: DEBUG ? [createLogger()] : [],
