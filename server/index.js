d83ed4479fc10485e4fe5c1dad222979eeb3bded
Ben Booth <bkbooth@gmail.com>
2018-06-08 22:17:03 +1000

    Use epilogue to create RESTful endpoints for models

--- server/index.js
+++ server/index.js
@@ -1,18 +1,34 @@
 const express = require('express')
 const cors = require('cors')
 const Sequelize = require('sequelize')
+const epilogue = require('epilogue')
+
+// Setup the express app
+let app = express()
+app.use(cors())
 
 // Setup the Sequelize database
 let sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: './db.sqlite',
 })
-let models = require('./models')(sequelize)
-console.log('MODELS', models)
+let {
+  Film,
+  Person,
+  Planet,
+  Species,
+  Starship,
+  Vehicle,
+} = require('./models')(sequelize)
 
-// Setup the express app
-let app = express()
-app.use(cors())
+// Setup epilogue REST API
+epilogue.initialize({ app, sequelize })
+epilogue.resource({ model: Film, endpoints: ['/films', '/films/:id'] })
+epilogue.resource({ model: Person, endpoints: ['/people', '/people/:id'] })
+epilogue.resource({ model: Planet, endpoints: ['/planets', '/planets/:id'] })
+epilogue.resource({ model: Species, endpoints: ['/species', '/species/:id'] })
+epilogue.resource({ model: Starship, endpoints: ['/starships', '/starships/:id'] })
+epilogue.resource({ model: Vehicle, endpoints: ['/vehicles', '/vehicles/:id'] })
 
 sequelize
   .sync({ force: true, logging: false })
