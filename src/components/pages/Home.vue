5617adf31e3a2ffafae849c22467062f8e310249
Ben Booth <bkbooth@gmail.com>
2018-06-13 11:58:57 +1000

    Reorder the homepage links

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -5,12 +5,12 @@
       <a href="https://swapi.co/">The Star Wars API</a>
     </p>
     <ul>
-      <li><router-link to="/people">People</router-link></li>
-      <li><router-link to="/planets">Planets</router-link></li>
       <li><router-link to="/films">Films</router-link></li>
+      <li><router-link to="/planets">Planets</router-link></li>
       <li><router-link to="/species">Species</router-link></li>
-      <li><router-link to="/vehicles">Vehicles</router-link></li>
+      <li><router-link to="/people">People</router-link></li>
       <li><router-link to="/starships">Starships</router-link></li>
+      <li><router-link to="/vehicles">Vehicles</router-link></li>
     </ul>
   </div>
 </template>
