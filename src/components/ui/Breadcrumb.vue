0a67985e63dc85ee4c2a2356959eda2efcce4295
Ben Booth <bkbooth@gmail.com>
2018-06-13 23:43:37 +1000

    Get the detail name from the vuex store

--- src/components/ui/Breadcrumb.vue
+++ src/components/ui/Breadcrumb.vue
@@ -7,26 +7,35 @@
       <fa-icon icon="home" size="xs"/> Home
     </router-link>
 
-    <span v-if="category">
+    <span v-if="categoryName">
       ▸
       <span v-if="isList">
-        <c-icon :category="category" size="xs"/>
-        {{ category | capitalize }}
+        <c-icon :category="categoryName" size="xs"/>
+        {{ categoryName | capitalize }}
       </span>
-      <router-link v-else :to="{ name: `${category}.list` }">
-        <c-icon :category="category" size="xs"/>
-        {{ category | capitalize }}
+      <router-link v-else :to="{ name: `${categoryName}.list` }">
+        <c-icon :category="categoryName" size="xs"/>
+        {{ categoryName | capitalize }}
       </router-link>
     </span>
 
     <span v-if="isDetail">
-      ▸ Detail
+      ▸ {{ detailName }}
     </span>
   </div>
 </template>
 
 <script>
 import capitalize from 'lodash/capitalize'
+
+const SLUGS = {
+  'films': 'filmSlug',
+  'planets': 'planetSlug',
+  'species': 'speciesSlug',
+  'people': 'personSlug',
+  'starships': 'starshipSlug',
+  'vehicles': 'vehicleSlug',
+}
 
 export default {
   name: 'Breadcrumb',
@@ -37,9 +46,19 @@ export default {
     routeName() {
       return this.$route.name || ''
     },
-    category() {
+    categoryName() {
       let [category, view] = this.routeName.split('.')
       return (view && category) || ''
+    },
+    detailName() {
+      if (this.isDetail) {
+        let getter = `${this.categoryName}/getItem`
+        let slugName = SLUGS[this.categoryName]
+        let slug = this.$route.params[slugName]
+        let detail = this.$store.getters[getter](slug)
+        if (detail) return detail.name || detail.title
+      }
+      return ''
     },
     isHome() {
       return this.routeName === 'home'
