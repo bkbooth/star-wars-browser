500d122ca3014dab53047d501d0f7dfc14071b55
Ben Booth <bkbooth@gmail.com>
2018-06-14 12:54:11 +1000

    Add a jsdoc comment for the objectify util

--- server/utils/objectify.js
+++ server/utils/objectify.js
@@ -1,2 +1,9 @@
+/**
+ * Maps an array of resource objects to an object with named resources
+ *
+ * @param {object[]} resources
+ *
+ * @returns {object}
+ */
 module.exports = ([films, planets, species, people, starships, vehicles]) =>
   ({ films, planets, species, people, starships, vehicles })
