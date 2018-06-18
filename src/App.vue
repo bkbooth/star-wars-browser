63e7ff6a2c84828984a2f71c0ca59ed774d76294
Ben Booth <bkbooth@gmail.com>
2018-06-18 10:54:47 +1000

    Fix z-index for header/breadcrumb layering

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="flex flex-col font-sans bg-grey-lightest text-grey-darkest min-h-screen">
-    <app-header class="flex-no-shrink bg-blue-darker text-grey-lightest px-4 lg:px-6 py-4 shadow-md"/>
+    <app-header class="flex-no-shrink bg-blue-darker text-grey-lightest px-4 lg:px-6 py-4 shadow-md z-10"/>
     <breadcrumb class="flex-no-shrink bg-white text-sm truncate px-4 lg:px-6 py-2 border-b"/>
     <div id="content" class="flex-1 p-4 lg:px-6">
       <router-view/>
