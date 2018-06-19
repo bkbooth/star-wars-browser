3429891533f1d56f7b0d4e673a6af61ca0fd8cb9
Ben Booth <bkbooth@gmail.com>
2018-06-19 11:32:57 +1000

    Use the data table component for the films list page

--- src/components/films/List.vue
+++ src/components/films/List.vue
@@ -4,15 +4,52 @@
 
     <alert v-if="error" :message="error"/>
 
-    <div v-if="count">
-      <p>Total: {{ count }}</p>
-      <ul>
-        <li v-for="film in filmsByEpisode" :key="film.id">
-          <router-link :to="`films/${film.slug}`">
-            Episode {{ film.episodeId }} - {{ film.title }}
-          </router-link>
-        </li>
-      </ul>
+    <div v-if="count" class="table-responsive -mt-8">
+      <p class="text-right">Total: {{ count }}</p>
+      <data-table
+        :data="films"
+        :order="order"
+        class="table table-striped table-hover"
+      >
+        <template slot-scope="{ rows }">
+          <caption>List of films</caption>
+          <thead>
+            <tr>
+              <th scope="col" class="whitespace-no-wrap">
+                <order-icon :order="order" field-name="episodeId" @set-order="setOrder"/>
+                Episode
+              </th>
+              <th scope="col" class="whitespace-no-wrap">
+                <order-icon :order="order" field-name="title" @set-order="setOrder"/>
+                Title
+              </th>
+              <th scope="col" class="whitespace-no-wrap">
+                <order-icon :order="order" field-name="releaseDate" @set-order="setOrder"/>
+                Released
+              </th>
+              <th scope="col" class="whitespace-no-wrap">
+                <order-icon :order="order" field-name="director" @set-order="setOrder"/>
+                Director
+              </th>
+              <th scope="col" class="whitespace-no-wrap">
+                <order-icon :order="order" field-name="producer" @set-order="setOrder"/>
+                Producer
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="film in rows" :key="film.id">
+              <td scope="row">{{ film.episodeId | romanize }}</td>
+              <td>
+                <router-link :to="`films/${film.slug}`">{{ film.title }}</router-link>
+              </td>
+              <td>{{ film.releaseDate | date('Do MMM YYYY') }}</td>
+              <td>{{ film.director }}</td>
+              <td>{{ film.producer }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </data-table>
     </div>
 
     <loading-spinner v-if="loading"/>
@@ -21,13 +58,18 @@
 
 <script>
 import { mapState, mapGetters } from 'vuex'
-import sortBy from 'lodash/sortBy'
+import DataTable from '@/components/ui/DataTable'
+import OrderIcon from '@/components/ui/OrderIcon'
 
 export default {
+  components: {
+    DataTable,
+    OrderIcon,
+  },
+  props: {
+    order: { type: String, default: 'episodeId' },
+  },
   computed: {
-    filmsByEpisode() {
-      return sortBy(this.films, 'episodeId')
-    },
     ...mapState('films', {
       films: 'data',
       loading: 'loading',
@@ -37,6 +79,13 @@ export default {
   },
   created() {
     this.$store.dispatch('films/loadMany')
+  },
+  methods: {
+    setOrder(fieldName, direction) {
+      let orderBy = `${direction === 'desc' ? '-' : ''}${fieldName}`
+      if (fieldName !== 'episodeId') orderBy += ',episodeId'
+      this.$router.push({ query: { orderBy } })
+    },
   },
 }
 </script>
--- src/router/films.js
+++ src/router/films.js
@@ -3,6 +3,7 @@ export default [
     path: '/films',
     name: 'films.list',
     component: () => import(/* webpackChunkName: "films" */ '@/components/films/List'),
+    props: (route) => ({ order: route.query.orderBy }),
   },
   {
     path: '/films/:slug',
