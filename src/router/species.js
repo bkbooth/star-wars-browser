f99625e8135157d6d7eaa3d3c92cddf8b82aeb28
Ben Booth <bkbooth@gmail.com>
2018-06-19 12:04:04 +1000

    Use data table component for species list page

--- src/router/species.js
+++ src/router/species.js
@@ -3,6 +3,7 @@ export default [
     path: '/species',
     name: 'species.list',
     component: () => import(/* webpackChunkName: "species" */ '@/components/species/List'),
+    props: (route) => ({ order: route.query.orderBy }),
   },
   {
     path: '/species/:slug',
