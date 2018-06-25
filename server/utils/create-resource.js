8977d87d70cbcb2c25751411f699a2b01f36fa5c
Ben Booth <bkbooth@gmail.com>
2018-06-26 00:44:31 +1000

    Handle order queries including 'random'

--- server/utils/create-resource.js
+++ server/utils/create-resource.js
@@ -17,14 +17,17 @@ module.exports = function createResource(Model) {
 
   router.get('/', (req, res, next) => {
     Model
-      .findAll()
+      .findAll({
+        order: parseOrderQuery(req.query.order),
+      })
       .then(records => res.status(200).json(records))
       .catch(err => next(err))
   })
 
   router.get('/:recordId', (req, res, next) => {
     Model
-      .findById(req.params.recordId)
+      .findById(req.params.recordId, {
+      })
       .then(record => {
         if (record == null) {
           return next(new HttpError(`Record ${Model.options.name.singular}:${req.params.recordId} not found`, 404))
@@ -36,4 +39,15 @@ module.exports = function createResource(Model) {
   })
 
   return router
+
+  function parseOrderQuery(orderQuery) {
+    if (orderQuery == null) return [['swapiId']]
+    else if (orderQuery === 'random') return [Model.sequelize.fn('RANDOM')]
+
+    let orderParts = orderQuery.split(',')
+    return orderParts.map(part => {
+      let [, direction, attribute] = /([-+]?)(\S+)/.exec(part)
+      return [attribute, direction === '-' ? 'DESC' : 'ASC']
+    })
+  }
 }
