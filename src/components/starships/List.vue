7ffdc6b8fb266ae786d842dccfdfcb76cd93fe5c
Ben Booth <bkbooth@gmail.com>
2018-07-04 22:21:33 +1000

    Darken the list page headers slightly

--- src/components/starships/List.vue
+++ src/components/starships/List.vue
@@ -2,7 +2,7 @@
   <page-wrapper>
     <template slot="main">
       <div class="bg-white rounded-lg shadow-lg my-6 p-6">
-        <h3 class="font-starjedi text-grey-dark -mt-1 mb-4">
+        <h3 class="font-starjedi text-grey-darker -mt-1 mb-4">
           <c-icon category="starships"/>
           Starships
         </h3>
