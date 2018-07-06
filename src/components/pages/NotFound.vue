a22e366b6eef4e30bff7ee393c12496661dd5a24
Ben Booth <bkbooth@gmail.com>
2018-07-07 01:08:15 +1000

    Tweak the not found page to give it some more personality

--- src/components/pages/NotFound.vue
+++ src/components/pages/NotFound.vue
@@ -1,15 +1,26 @@
 <template>
   <page-wrapper hero-bg-color="red-dark">
     <template slot="hero">
-      <h1 class="font-starjedi">
-        <fa-icon :icon="['far', 'exclamation-triangle']"/>
-        Page Not Found
-      </h1>
+      <div class="flex items-center">
+        <div class="flex-no-shrink -ml-1 mr-4">
+          <fa-icon :icon="['far', 'sad-tear']" class="text-3xl"/>
+        </div>
+        <div class="flex-grow">
+          <h1 class="font-starjedi">Page Not Found</h1>
+        </div>
+      </div>
     </template>
 
     <template slot="main">
-      <p>Sorry! We couldn't find the page you are looking for.</p>
-      <p><router-link to="/">Go to home</router-link></p>
+      <p class="my-8">
+        Sorry! I tried my hardest but I couldn't find the page you are looking for.
+      </p>
+      <p>
+        <router-link to="/">
+          <fa-icon :icon="['far', 'home']"/>
+          Go to the homepage
+        </router-link>
+      </p>
     </template>
   </page-wrapper>
 </template>
